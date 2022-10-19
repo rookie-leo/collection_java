495ced790af079679b7e2ad6d8b4714d7a38420a
rookie-leo <proleonarpa@gmail.com>
2022-10-19 11:19:21 -0300

    Trabalhando com Arrays

---
+++ src/TestandoListaDeAula.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class TestandoListaDeAula {
+
+    public static void main(String[] args) {
+        Aula a1 = new Aula("Revisando as ArraysList", 21);
+        Aula a2 = new Aula("Lista de Objetos", 25);
+        Aula a3 = new Aula("Relacionamento de Listas e Objetos", 15);
+
+        ArrayList<Aula> aulas = new ArrayList<>();
+    }
+
+}
---
+++ src/TestandoListas.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
+
+    public static void main(String[] args) {
+        String aula1 = "Conhecendo mais de listas";
+        String aula2 = "Modelando a classe Aula";
+        String aula3 = "Trabalhando com Cursos e Sets";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+        System.out.println(aulas);
+        System.out.println("////////////////");
+
+        aulas.remove(0);
+        System.out.println(aulas);
+        System.out.println("////////[ for each ]////////");
+        for (String aula : aulas) {
+            System.out.println("Aula " + aula);
+        }
+
+        String primeiraAula = aulas.get(0);
+        System.out.println("Primeira aula" + primeiraAula);
+        System.out.println("///////[ for ]/////////");
+        for (int i = 0; i < aulas.size(); i++) {
+            System.out.println(aulas.get(i));
+        }
+
+        System.out.println("///////[ forEach ]/////////");
+        aulas.forEach(aula -> {
+            System.out.println("Aula " + aula);
+        });
+
+        aulas.add("Aumentando nosso conhecimento");
+
+        System.out.println("Antes de ordernar: ");
+        System.out.println(aulas);
+
+        System.out.println("Depois de ordernar: ");
+        Collections.sort(aulas);
+        System.out.println(aulas);
+    }
+}
